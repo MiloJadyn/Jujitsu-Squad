64094c725a52dfa199fad5b7f8d0070fad492750
Jadyn Milo <milojadyn@gmail.com>
2025-11-10 21:48:10 -0500

    Update - JM 1
    Update to GameManager and Ememy 1 from Jadyn Milo for Task 1 for Week 12 completion.

--- GameManager.cs
+++ GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     public GameObject coinPrefab;
 
     public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
 
     public float horizontalScreenSize;
     public float verticalScreenSize;
@@ -59,9 +60,12 @@ public class GameManager : MonoBehaviour
         }
         
     }
+
+   
     public void AddScore(int earnedScore)
     {
         score = score + earnedScore;
+        scoreText.text = "Score: " + score;
     }
 
     public void ChangeLivesText (int currentLives)
